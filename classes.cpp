609f89f200f07b6252020e44636398b1cc11fd72
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2018-03-27 00:21:53 -0300

    Começo dos trabalhos

---
+++ classes.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <stdio.h>
+
+class Dominios{
+	public:
+		void validar(string);
+}
+
+class Dominios : public Nome{
+	string nome;
+};
+
+void Nome::validar(string nome){
+
+}
+
+class telefone{
+			
+};

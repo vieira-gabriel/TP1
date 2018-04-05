6fc2788b74400c4e90c840707795fb0f936fd82a
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2018-04-05 10:41:07 -0300

    Criado arquivos para teste e definida classe Leitor no header
    Os arquivos testes estão vazios. Apaguei praticamente tudo que estava no header de Entidades.hpp para começar a definir da forma correta

--- Entidades.hpp
+++ Entidades.hpp
@@ -1,72 +1,64 @@
-#include <string>
-#include <stdio.h>
+
 
 class Leitor{ //Dados requeridos para leitor (usuário mais básico)
 private:
-	string nome, sobrenome, email, senha;
+	Nome nome, sobrenome;
+	Email email;
+	Senha senha;
+	void validarNome(Nome);	//Essa classe também vale para validar o sobrenome
+	void validarEmail(Email);
+	void validarSenha(Senha);
 
 public:
 	Leitor();
-	void setNome(string nome);
-	void setSobrenome(string sobrenome);
-	void setEmail(string email);
-	void setSenha(string senha);	
+	void setNome(Nome nome);
+	void setSobrenome(Nome sobrenome);
+	void setEmail(Email email);
+	void setSenha(Senha senha);	
 };
 
 class Desenvolvedor : public Leitor{
 private:
-	string nascimento;
+	
 
 public:
 	Desenvolvedor();
-	void setNascimento(string nascimento);
+	
 };
 
 class Administrador : public Desenvolvedor{
 public:
-	int telefone;
-	string endereco;
+	
 
 private:
 	Administrador();
-	void setTelefone(int telefone);
-	void setEndereço(string endereco);
 	
 };
 
 class Data{  //data de criação das entidades abaixo
 private:
-	string data;
+	
 
 public:
 	Data();
-	void setData(string data);
 };
 
 class Vocabilario : public Data{
 private:
-	string nome, idioma;
 
 public:
 	Vocabilario();
-	void setNome(string nome);
-	void setIdioma(string idioma);
 };
 
 class Termo : public Data{
 private:
-	string nome, classe
 
 public:
 	Termo();
-	void setNome (string nome);
-	void setClasse (string classe);
 };
 
 class Definição : public Data{
 private:
-	string texto;
 public:
 	Definição();
-	void setTexto (string texto);	
 };
